3b9b5776f91123bf47be618148b3cae6627a6692
Jefte Santos <jeftenunes@hotmail.com>
2018-02-05 15:10:09 -0200

    Learning how to exchange data between components, events and so on.

--- src/StudentForm.jsx
+++ src/StudentForm.jsx
@@ -1,3 +1,4 @@
+import StudentList from './StudentList';
 import React, { Component } from 'react';
 
 class StudentForm extends Component {
@@ -6,18 +7,45 @@ class StudentForm extends Component {
 
         this.state = {
             name: '',
-            age: null
+            age: null,
+            students: []
         };
     }
 
     render() {
         return(
             <div>
-                <input type="text" name="name" placeholder="Type the name"></input>
-                <input type="number" name="age" placeholder="Type the age"></input>
-                <button>Add</button>
+                <input type="text" name="name" placeholder="Type the name" onChange={(evt) => this.updateName(evt)}></input>
+                <input type="number" name="age" placeholder="Type the age" onChange={(evt) => this.updateAge(evt)}></input>
+                <button type="button" onClick={(evt) => this.insertUser(evt)}>Add</button>
+                <StudentList students={this.state.students}/>
             </div>
         );
+    }
+
+    updateName = (e) => {
+        console.log(e);
+        return this.setState({
+            name: e.target.value
+        });
+    }
+
+    updateAge = (e) => {
+        return this.setState({
+            age: e.target.value
+        });
+    }
+
+    insertUser = (e) => {
+        this.state.students.push({
+            name: this.state.name,
+            age: this.state.age
+        });
+
+        return this.setState({
+            age: null,
+            name: ''
+        });
     }
 }
 
--- src/StudentList.jsx
+++ src/StudentList.jsx
@@ -3,13 +3,35 @@ import React, { Component } from 'react';
 class StudentList extends Component {
     constructor(props) {
         super(props);
+        
+        this.state = {
+            students: this.props.students
+        };
     }
 
     render() {
-        <div>
-            <ul>
-                <il></il>
-            </ul>
-        </div>
+        const students = this.state.students.map((student) => {
+            return <li key={student.name}>{student.name} - {student.age}<button type="button" onClick={(evt) => this.removeItem(student)}>Remover</button></li>    
+        });
+
+        return(
+            <div>
+                <ul>
+                    {students}
+                </ul>
+            </div>
+        );
+    }
+
+    removeItem = (student) => {
+        const newState = this.state.students;
+        if(newState.indexOf(student) > -1) {
+            newState.splice(newState.indexOf(student), 1);
+            this.setState({
+                students: newState
+            });
+        }
     }
 }
+
+export default StudentList;
